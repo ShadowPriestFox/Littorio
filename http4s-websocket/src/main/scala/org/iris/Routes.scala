31e4a361cfd18b49ad8f67b8418034802ce55426
hxx0215 <hxx0215@gmail.com>
2024-03-22 14:12:34 +0000

    [http4s-websocket] chat html

--- http4s-websocket/src/main/scala/org/iris/Routes.scala
+++ http4s-websocket/src/main/scala/org/iris/Routes.scala
@@ -2,6 +2,7 @@ package org.iris
 
 import fs2.io.file.Files
 import cats.MonadThrow
+import cats.effect.Concurrent
 import org.http4s.dsl.Http4sDsl
 import org.http4s.HttpApp
 import org.http4s.HttpRoutes
@@ -10,8 +11,9 @@ import fs2.Stream
 import fs2.Pipe
 import org.http4s.websocket.WebSocketFrame
 import org.http4s.server.websocket.WebSocketBuilder2
+import cats.effect.std.Queue
 
-class Routes[F[_]: Files: MonadThrow] extends Http4sDsl[F] {
+class Routes[F[_]: Files: Concurrent] extends Http4sDsl[F] {
   def service(wsb: WebSocketBuilder2[F]): HttpApp[F] = 
     HttpRoutes.of[F]{
       case request @ GET -> Root / "chat.html" => 
@@ -20,6 +22,7 @@ class Routes[F[_]: Files: MonadThrow] extends Http4sDsl[F] {
           Some(request)
         ).getOrElseF(NotFound())
       case GET -> Root / "ws" => 
+        val wrappedQueue: F[Queue[F, WebSocketFrame]] = Queue.unbounded[F, WebSocketFrame]
         val send: Stream[F, WebSocketFrame] = ???
         val receive: Pipe[F, WebSocketFrame, Unit] = ???
         wsb.build(send, receive)
