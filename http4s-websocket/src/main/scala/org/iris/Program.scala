4eb7f4e5ce010bd05b4d4995640d955627f3ed59
hxx <hxx0215@gmail.com>
2024-03-28 13:33:34 +0000

    [http4s-websocket] server finish

--- http4s-websocket/src/main/scala/org/iris/Program.scala
+++ http4s-websocket/src/main/scala/org/iris/Program.scala
@@ -8,15 +8,19 @@ import org.http4s.websocket.WebSocketFrame
 import fs2.concurrent.Topic
 import fs2.Stream 
 import concurrent.duration.DurationInt
+import cats.effect.kernel.Ref
 
 object  Program extends IOApp.Simple{
   override def run: IO[Unit] = for {
-    q <- Queue.unbounded[IO, WebSocketFrame]
-    t <- Topic[IO, WebSocketFrame]
+    q <- Queue.unbounded[IO, OutputMessage]
+    t <- Topic[IO, OutputMessage]
+    cs <- Ref.of[IO, ChatState](ChatState(Map.empty,Map.empty))
+    protocol <- IO(Protocol.make[IO](cs))
+    im <- IO(InputMessage.make[IO](protocol))
     s <- Stream(
       Stream.fromQueueUnterminated(q).through(t.publish),
-      Stream.awakeEvery[IO](30.seconds).map(_ => WebSocketFrame.Ping()).through(t.publish),
-      Stream.eval(server[IO](q,t))
+      Stream.awakeEvery[IO](30.seconds).map(_ => KeepAlive).through(t.publish),
+      Stream.eval(server[IO](q,t,im,protocol,cs))
     ).parJoinUnbounded.compile.drain
   } yield s
 }
