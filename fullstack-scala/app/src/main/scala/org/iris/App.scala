8e9100e8c48175116f9dfa13f8f75d4e4ee9847a
hxx <hxx0215@gmail.com>
2024-04-07 18:25:13 +0000

    [fullstack-scala] finish

--- fullstack-scala/app/src/main/scala/org/iris/App.scala
+++ fullstack-scala/app/src/main/scala/org/iris/App.scala
@@ -5,22 +5,53 @@ import scala.scalajs.js.annotation.*
 import tyrian.TyrianApp
 import tyrian.Cmd
 import cats.effect.IO
-import tyrian.Html
-import tyrian.Sub
+import org.iris.domain.Job
+import tyrian.*
+import tyrian.Html.*
+import tyrian.http.*
+import cats.implicits.*
+import io.circe.syntax.*
+import io.circe.parser.*
+import io.circe.generic.auto.*
 
-trait Msg
-trait Model
+enum Msg:
+  case NoMsg
+  case LoadJobs(jobs: List[Job])
+  case Error(e: String)
+
+case class Model(jobs: List[Job] = List())
 
 @JSExportTopLevel("IrisApp")
 object App extends TyrianApp[Msg, Model]:
 
-  override def init(flags: Map[String, String]): (Model, Cmd[IO, Msg]) = ???
+  def backendCall: Cmd[IO, Msg] = Http.send(
+    Request.get("http://localhost:4041/jobs"),
+    Decoder[Msg](
+      resp => {
+        parse(resp.body).flatMap(_.as[List[Job]]) match
+          case Left(e) => Msg.Error(e.getMessage())
+          case Right(list) => Msg.LoadJobs(list)
+      },
+      err => Msg.Error(err.toString))
+  )
 
-  override def view(model: Model): Html[Msg] = ???
+  override def init(flags: Map[String, String]): (Model, Cmd[IO, Msg]) = (Model(), backendCall)
 
-  override def update(model: Model): Msg => (Model, Cmd[IO, Msg]) = ???
+  override def view(model: Model): Html[Msg] = 
+    div(`class` := "row")(
+      p("This is the first ScalaJS app by iris"),
+      div(`class` := "contents")(
+        model.jobs.map: job=>
+          div(job.toString())
+      )
+    )
 
-  override def subscriptions(model: Model): Sub[IO, Msg] = ???
+  override def update(model: Model): Msg => (Model, Cmd[IO, Msg]) = 
+    case Msg.NoMsg => (model, Cmd.None)
+    case Msg.Error(e) => (model, Cmd.None)
+    case Msg.LoadJobs(job) => (model.copy(model.jobs ++ job), Cmd.None)
+
+  override def subscriptions(model: Model): Sub[IO, Msg] = Sub.None
     
 
   
