87397974fb668a0acbdd562022f4fd89c556ba38
hxx <hxx0215@gmail.com>
2024-04-09 16:24:13 +0000

    [typelevel-program] encoder

--- typelevel-prog/typelevel-program.scala
+++ typelevel-prog/typelevel-program.scala
@@ -1,6 +1,7 @@
 import io.circe.* 
 import io.circe.generic.semiauto
 import cats.implicits.*
+import scala.compiletime.summonInline
 
 case class Phone(number: String, prefix: Int)
 case class Email(primary: String, secondary: Option[String])
@@ -13,6 +14,9 @@ object Email:
 object Phone:
   given Codec.AsObject[Phone] = semiauto.deriveCodec[Phone]
 
+object Address:
+  given Codec.AsObject[Address] = semiauto.deriveCodec[Address]
+
 val user = User(
   Email(
     "hxx@iris.org",
@@ -22,3 +26,24 @@ val user = User(
   Address("Ca", "Laval")
 )
 
+def listToJson[A: Encoder](ls: List[A]): List[Json] = 
+  ls match
+    case Nil => Nil
+    case h :: t => 
+      val encoder = summon[Encoder[A]]
+      val json = encoder(h)
+      json :: listToJson(t)
+
+inline def tupleToJson(tuple: Tuple): List[JsonObject] =
+  inline tuple match
+    case EmptyTuple => Nil
+    case tup: (h *: t) => 
+      val encoder = summonInline[Encoder.AsObject[h]]
+      val json = encoder.encodeObject(tup.head)
+      json :: tupleToJson(tup.tail)
+
+def concatObjects(jsons: List[JsonObject]): Json = Json.obj(jsons.flatMap(_.toList): _*)
+val encoder = Encoder.instance[User]: value =>
+  val fields = Tuple.fromProductTyped(value)
+  val jsons = tupleToJson(fields)
+  concatObjects(jsons)
