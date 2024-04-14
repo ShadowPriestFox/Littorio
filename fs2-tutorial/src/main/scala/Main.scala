6b1b54837d8370d9f5a235a731a93ab8f119a0f7
hxx <hxx0215@gmail.com>
2024-04-14 02:09:03 +0000

    [fs2] basic data

--- fs2-tutorial/src/main/scala/Main.scala
+++ fs2-tutorial/src/main/scala/Main.scala
@@ -1,3 +1,4 @@
+import Model.Actor
 @main def hello(): Unit =
   println("Hello world!")
   println(msg)
@@ -5,3 +6,12 @@
 def msg = "I was compiled by Scala 3. :)"
 object Model:
   case class Actor(id: Int, firstName: String, lastName: String)
+
+object Data:
+  val henryCavil: Actor = Actor(0, "Henry", "Cavil")
+  val galGodot: Actor = Actor(1, "Gal", "Godot")
+  val ezraMiller: Actor = Actor(2, "Ezra", "Miller")
+  val benFisher: Actor = Actor(3, "Ben", "Fisher")
+  val rayHardy: Actor = Actor(4, "Ray", "Hardy")
+  val jasonMomoa: Actor = Actor(5, "Jason", "Momoa")
+  val scarlettJohansson: Actor = Actor(6, "Scarlett", "Johansson")
