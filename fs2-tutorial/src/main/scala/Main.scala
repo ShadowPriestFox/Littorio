7a18b4fd507110f2caeef9e1cdcfa11ab70e2b76
hxx <hxx0215@gmail.com>
2024-04-18 02:52:19 +0000

    [fs2-tutorial] some stream feat

--- fs2-tutorial/src/main/scala/Main.scala
+++ fs2-tutorial/src/main/scala/Main.scala
@@ -1,7 +1,20 @@
 import Model.Actor
+import fs2.Stream
+import fs2.Pure
+import Data.*
 @main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
+  val jlActors: Stream[Pure, Actor] = Stream(
+    henryCavil,
+    galGodot,
+    ezraMiller,
+    benFisher,
+    rayHardy,
+    jasonMomoa
+  )
+  val tomHollandStream: Stream[Pure, Actor] = Stream.emit(tomHolland)
+  val jlActorList: List[Actor] = jlActors.toList
+  val jlActorVector: Vector[Actor] = jlActors.toVector
+  val infiniteJLActors: Stream[Pure, Actor] = jlActors.repeat
 
 def msg = "I was compiled by Scala 3. :)"
 object Model:
@@ -15,3 +28,11 @@ object Data:
   val rayHardy: Actor = Actor(4, "Ray", "Hardy")
   val jasonMomoa: Actor = Actor(5, "Jason", "Momoa")
   val scarlettJohansson: Actor = Actor(6, "Scarlett", "Johansson")
+  val robertDowneyJr: Actor = Actor(7,"Robert", "Downey Jr.")
+  val chrisEvans: Actor = Actor(8,"Chris", "Evans")
+  val markRuffalo: Actor = Actor(9, "Mark", "Ruffalo")
+  val chrisHemsworth: Actor = Actor(10, "Chris", "Hemsworth")
+  val jeremyRenner: Actor = Actor(11, "Jeremy", "Renner")
+  val tomHolland: Actor = Actor(12, "Tom", "Holland")
+  val tobeyMaguire: Actor = Actor(13, "Tobey", "Maguire")
+  val andrewGarfield: Actor = Actor(14, "Anndrew", "Garfield")
