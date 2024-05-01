dfc29dde0a443d6ba38fa46b49ccb29f47903200
hxx0215 <hxx0215@gmail.com>
2024-05-01 12:51:20 -0400

    [feat] add execute command

--- mainz/src/main/scala/org/iris/mainz/service/CommandExecutorService.scala
+++ mainz/src/main/scala/org/iris/mainz/service/CommandExecutorService.scala
@@ -3,12 +3,20 @@ package org.iris.mainz.service
 import org.iris.mainz.domain.ExecuteContext
 import org.iris.mainz.domain.ProcessInstanceInfo
 import org.iris.mainz.domain.ExecutorResponse
-import cats.effect.kernel.Async
+import cats.effect.Async
+import cats.implicits.*
 
 trait CommandExecutorService[F[_]]:
   def execute(context: ExecuteContext,info: ProcessInstanceInfo,topicName: String): F[ExecutorResponse]
 
 object CommandExecutorService:
   def make[F[_]: Async](taskService: TaskService[F]) = new CommandExecutorService[F] {
-    override def execute(context: ExecuteContext, info: ProcessInstanceInfo, topicName: String): F[ExecutorResponse] = ???
+    override def execute(context: ExecuteContext, info: ProcessInstanceInfo, topicName: String): F[ExecutorResponse] =
+      for
+        task <- taskService.findByTopicAndFlow(topicName,info.processDefinitionId)
+      yield ???
+
+    def executeCommand(commandId: String,info: ProcessInstanceInfo) = ???
   }
+
+
